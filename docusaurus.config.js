50796e7ce3df740b0ce78fe2b189b30e63432b7b
tadashi.shimoda <t.shimoda012@gmail.com>
2024-04-26 14:44:57 +0900

    Fintanロゴからfintanへリンクするよう修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://extreal-dev.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/extreal-r3f-guide",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "extreal-dev", // Usually your GitHub org/user name.
+  projectName: "extreal-r3f-guide", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -42,17 +42,9 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -71,6 +63,7 @@ const config = {
         logo: {
           alt: "Fintan Logo",
           src: "img/fintan-logo.jpg",
+          href: "https://fintan.jp",
         },
         items: [
           {
